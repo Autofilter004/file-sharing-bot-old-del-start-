73411fd4a9ddc0b07a7275b64184588a3e990f34
Mhd Thanzeer 004 <87107399+Autofilter004@users.noreply.github.com>
2021-12-13 19:41:33 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -6,7 +6,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import FloodWait
 
 from bot import Bot
-from config import ADMINS, START_MSG, OWNER_ID, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON
+from config import ADMINS, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON
 from helper_func import subscribed, encode, decode, get_messages
 
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
