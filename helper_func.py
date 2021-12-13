b6387297495e29dc1a1923242b51496077b64c02
Mhd Thanzeer 004 <87107399+Autofilter004@users.noreply.github.com>
2021-12-13 20:58:27 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -4,37 +4,11 @@ import base64
 import re
 import asyncio
 from pyrogram import filters
-from config import FORCE_SUB_CHANNEL, ADMINS
+from config import ADMINS
 from pyrogram.errors.exceptions.bad_request_400 import UserNotParticipant
 from pyrogram.errors import FloodWait
 
-async def is_subscribed(filter, client, message):
-    if not FORCE_SUB_CHANNEL:
-        return True
-    user_id = message.from_user.id
-    if user_id in ADMINS:
-        return True
-    try:
-        member = await client.get_chat_member(chat_id = FORCE_SUB_CHANNEL, user_id = user_id)
-    except UserNotParticipant:
-        return False
 
-    if not member.status in ["creator", "administrator", "member"]:
-        return False
-    else:
-        return True
-
-async def encode(string):
-    string_bytes = string.encode("ascii")
-    base64_bytes = base64.b64encode(string_bytes)
-    base64_string = base64_bytes.decode("ascii")
-    return base64_string
-
-async def decode(base64_string):
-    base64_bytes = base64_string.encode("ascii")
-    string_bytes = base64.b64decode(base64_bytes) 
-    string = string_bytes.decode("ascii")
-    return string
 
 async def get_messages(client, message_ids):
     messages = []
@@ -82,4 +56,4 @@ async def get_message_id(client, message):
     else:
         return 0
 
-subscribed = filters.create(is_subscribed)
+
