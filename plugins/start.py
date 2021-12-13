7b9d7488a7ec5d8f9a1b5be68b5799ae7c171f04
Mhd Thanzeer 004 <87107399+Autofilter004@users.noreply.github.com>
2021-12-13 19:25:57 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -69,29 +69,7 @@ async def start_command(client: Client, message: Message):
             except:
                 pass
         return
-    else:
-        reply_markup = InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton("😊 About Me", callback_data = "about"),
-                    InlineKeyboardButton("🔒 Close", callback_data = "close")
-                ]
-            ]
-        )
-        await message.reply_text(
-            text = START_MSG.format(
-                first = message.from_user.first_name,
-                last = message.from_user.last_name,
-                username = None if not message.from_user.username else '@' + message.from_user.username,
-                mention = message.from_user.mention,
-                id = message.from_user.id
-            ),
-            reply_markup = reply_markup,
-            disable_web_page_preview = True,
-            quote = True
-        )
-        return
-
+    
 @Bot.on_message(filters.command('start') & filters.private)
 async def not_joined(client: Client, message: Message):
     text = "<b>You need to join in my Channel/Group to use me\n\nKindly Please join Channel</b>"
